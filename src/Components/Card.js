3047fd62c8dea3e8512f8cfc0f380a2e44924f8c
Yasir <yasir.mansoori000@gmail.com>
2023-09-18 03:15:49 +0530

    Have an issue button with modal implemetation

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -38,8 +38,68 @@ export default function Card() {
     return (
         <>
             <h1 className="text-center">Create your own QR!</h1>
+            {/* have an issue button with modal */}
+            <button
+                id="button"
+                type="button"
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={20}
+                    viewBox="0 0 20 20"
+                    height={20}
+                    fill="none"
+                    className="svg-icon"
+                >
+                    <g strokeWidth="1.5" strokeLinecap="round" stroke="#5d41de">
+                        <circle r="2.5" cy={10} cx={10} />
+                        <path
+                            fillRule="evenodd"
+                            d="m8.39079 2.80235c.53842-1.51424 2.67991-1.51424 3.21831-.00001.3392.95358 1.4284 1.40477 2.3425.97027 1.4514-.68995 2.9657.82427 2.2758 2.27575-.4345.91407.0166 2.00334.9702 2.34248 1.5143.53842 1.5143 2.67996 0 3.21836-.9536.3391-1.4047 1.4284-.9702 2.3425.6899 1.4514-.8244 2.9656-2.2758 2.2757-.9141-.4345-2.0033.0167-2.3425.9703-.5384 1.5142-2.67989 1.5142-3.21831 0-.33914-.9536-1.4284-1.4048-2.34247-.9703-1.45148.6899-2.96571-.8243-2.27575-2.2757.43449-.9141-.01669-2.0034-.97028-2.3425-1.51422-.5384-1.51422-2.67994.00001-3.21836.95358-.33914 1.40476-1.42841.97027-2.34248-.68996-1.45148.82427-2.9657 2.27575-2.27575.91407.4345 2.00333-.01669 2.34247-.97026z"
+                            clipRule="evenodd"
+                        />
+                    </g>
+                </svg>
+                <span className="lable">Have an Issue ?</span>
+            </button>
+            {/* Modal */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <form action="https://formsubmit.co/yasir.mansoori000@gmail.com" method="POST">
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">Enter Your Full Name</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" name="name" aria-describedby="emailHelp" placeholder='John Doe' />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder='JohnDoe@mail.com' />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputurl" className="form-label">URL: </label>
+                                    <input type="email" className="form-control" id="exampleInputurl" name="url" aria-describedby="emailHelp" placeholder='https://www.example.com' />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleFormControlTextarea1" className="form-label">Report your issue</label>
+                                    <textarea className="form-control" id="exampleFormControlTextarea1" name='textarea' rows="3"></textarea>
+                                </div>
+                                <button type="submit" name="_next" value="https://qr-code-generetor.netlify.app"><span class="box">Submit</span></button>
+                                <button type="button" id='close' data-bs-dismiss="modal"><span class="box">Close</span></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* have an issue button with modal end */}
+
             <div className="container">
-                <div className="card">
+                {/* Card implementation  */}
+                <div className="card" id='card'>
                     <img src={qrCode} className="card-img-top" alt="..." />
                     <div className="card-body text-center">
                         <div className="card-inner-body">
@@ -84,6 +144,8 @@ export default function Card() {
                         </div>
                     </div>
                 </div>
+                {/* Card implementation end */}
+
                 {/* Toast ADD-ON */}
                 <div className="toast" id='toast' role="alert" aria-live="assertive" aria-atomic="true">
                     <div className="toast-header">
